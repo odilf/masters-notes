43aeffb30a09c7c24de98f2f5f9fe408f8bce119
odilf <odysseas.maheras@gmail.com>
2026-03-14 12:24:03 +0100

    add font to site

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import '../app.css';
+	import '@fontsource/iosevka/300';
+	import '@fontsource/iosevka/400';
+	import '@fontsource/iosevka/700';
+	import '@fontsource/iosevka/900';
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
